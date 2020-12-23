9745f615db3726063968a730ac81daa5cd85639a
Antonio Meraz <72181725+antonio-meraz-villa@users.noreply.github.com>
2020-12-23 03:29:02 -0600

    First base for the game
    This game is now functioning

---
+++ Pinball proof of concept/Pinball proof of concept/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿
+namespace Pinball_proof_of_concept
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.paletaizq = new System.Windows.Forms.PictureBox();
+            this.paletader = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.score = new System.Windows.Forms.Label();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            this.barraizq = new System.Windows.Forms.PictureBox();
+            this.barrader = new System.Windows.Forms.PictureBox();
+            this.barratop = new System.Windows.Forms.PictureBox();
+            this.objsid3 = new System.Windows.Forms.PictureBox();
+            this.objdwn2 = new System.Windows.Forms.PictureBox();
+            this.objsid4 = new System.Windows.Forms.PictureBox();
+            this.objtop2 = new System.Windows.Forms.PictureBox();
+            this.objtop1 = new System.Windows.Forms.PictureBox();
+            this.objsid2 = new System.Windows.Forms.PictureBox();
+            this.objdwn1 = new System.Windows.Forms.PictureBox();
+            this.objsid1 = new System.Windows.Forms.PictureBox();
+            this.objtop3 = new System.Windows.Forms.PictureBox();
+            this.objsid6 = new System.Windows.Forms.PictureBox();
+            this.objdwn3 = new System.Windows.Forms.PictureBox();
+            this.objsid5 = new System.Windows.Forms.PictureBox();
+            this.limit = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.paletaizq)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paletader)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barraizq)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barrader)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barratop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objsid3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objdwn2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objsid4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objtop2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objtop1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objsid2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objdwn1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objsid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objtop3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objsid6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objdwn3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objsid5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.limit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // paletaizq
+            // 
+            this.paletaizq.BackColor = System.Drawing.Color.SpringGreen;
+            this.paletaizq.Location = new System.Drawing.Point(40, 407);
+            this.paletaizq.Name = "paletaizq";
+            this.paletaizq.Size = new System.Drawing.Size(100, 19);
+            this.paletaizq.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.paletaizq.TabIndex = 0;
+            this.paletaizq.TabStop = false;
+            // 
+            // paletader
+            // 
+            this.paletader.BackColor = System.Drawing.Color.SpringGreen;
+            this.paletader.Location = new System.Drawing.Point(219, 407);
+            this.paletader.Name = "paletader";
+            this.paletader.Size = new System.Drawing.Size(100, 19);
+            this.paletader.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.paletader.TabIndex = 1;
+            this.paletader.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Linen;
+            this.ball.Location = new System.Drawing.Point(107, 336);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.Location = new System.Drawing.Point(133, 33);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(99, 23);
+            this.score.TabIndex = 6;
+            this.score.Text = "Score: 0";
+            // 
+            // gametimer
+            // 
+            this.gametimer.Enabled = true;
+            this.gametimer.Interval = 20;
+            this.gametimer.Tick += new System.EventHandler(this.gametimer_Tick);
+            // 
+            // barraizq
+            // 
+            this.barraizq.BackColor = System.Drawing.Color.Maroon;
+            this.barraizq.Location = new System.Drawing.Point(12, 9);
+            this.barraizq.Name = "barraizq";
+            this.barraizq.Size = new System.Drawing.Size(19, 449);
+            this.barraizq.TabIndex = 7;
+            this.barraizq.TabStop = false;
+            // 
+            // barrader
+            // 
+            this.barrader.BackColor = System.Drawing.Color.Maroon;
+            this.barrader.Location = new System.Drawing.Point(330, 9);
+            this.barrader.Name = "barrader";
+            this.barrader.Size = new System.Drawing.Size(19, 449);
+            this.barrader.TabIndex = 8;
+            this.barrader.TabStop = false;
+            // 
+            // barratop
+            // 
+            this.barratop.BackColor = System.Drawing.Color.Maroon;
+            this.barratop.Location = new System.Drawing.Point(12, 9);
+            this.barratop.Name = "barratop";
+            this.barratop.Size = new System.Drawing.Size(337, 19);
+            this.barratop.TabIndex = 9;
+            this.barratop.TabStop = false;
+            // 
+            // objsid3
+            // 
+            this.objsid3.BackColor = System.Drawing.Color.Gold;
+            this.objsid3.Location = new System.Drawing.Point(221, 88);
+            this.objsid3.Name = "objsid3";
+            this.objsid3.Size = new System.Drawing.Size(10, 50);
+            this.objsid3.TabIndex = 10;
+            this.objsid3.TabStop = false;
+            // 
+            // objdwn2
+            // 
+            this.objdwn2.BackColor = System.Drawing.Color.Gold;
+            this.objdwn2.Location = new System.Drawing.Point(231, 136);
+            this.objdwn2.Name = "objdwn2";
+            this.objdwn2.Size = new System.Drawing.Size(50, 10);
+            this.objdwn2.TabIndex = 11;
+            this.objdwn2.TabStop = false;
+            // 
+            // objsid4
+            // 
+            this.objsid4.BackColor = System.Drawing.Color.Gold;
+            this.objsid4.Location = new System.Drawing.Point(279, 88);
+            this.objsid4.Name = "objsid4";
+            this.objsid4.Size = new System.Drawing.Size(10, 50);
+            this.objsid4.TabIndex = 12;
+            this.objsid4.TabStop = false;
+            // 
+            // objtop2
+            // 
+            this.objtop2.BackColor = System.Drawing.Color.Gold;
+            this.objtop2.Location = new System.Drawing.Point(231, 80);
+            this.objtop2.Name = "objtop2";
+            this.objtop2.Size = new System.Drawing.Size(50, 10);
+            this.objtop2.TabIndex = 13;
+            this.objtop2.TabStop = false;
+            // 
+            // objtop1
+            // 
+            this.objtop1.BackColor = System.Drawing.Color.Gold;
+            this.objtop1.Location = new System.Drawing.Point(78, 136);
+            this.objtop1.Name = "objtop1";
+            this.objtop1.Size = new System.Drawing.Size(50, 10);
+            this.objtop1.TabIndex = 17;
+            this.objtop1.TabStop = false;
+            // 
+            // objsid2
+            // 
+            this.objsid2.BackColor = System.Drawing.Color.Gold;
+            this.objsid2.Location = new System.Drawing.Point(126, 146);
+            this.objsid2.Name = "objsid2";
+            this.objsid2.Size = new System.Drawing.Size(10, 50);
+            this.objsid2.TabIndex = 16;
+            this.objsid2.TabStop = false;
+            // 
+            // objdwn1
+            // 
+            this.objdwn1.BackColor = System.Drawing.Color.Gold;
+            this.objdwn1.Location = new System.Drawing.Point(77, 195);
+            this.objdwn1.Name = "objdwn1";
+            this.objdwn1.Size = new System.Drawing.Size(50, 10);
+            this.objdwn1.TabIndex = 15;
+            this.objdwn1.TabStop = false;
+            // 
+            // objsid1
+            // 
+            this.objsid1.BackColor = System.Drawing.Color.Gold;
+            this.objsid1.Location = new System.Drawing.Point(68, 146);
+            this.objsid1.Name = "objsid1";
+            this.objsid1.Size = new System.Drawing.Size(10, 50);
+            this.objsid1.TabIndex = 14;
+            this.objsid1.TabStop = false;
+            // 
+            // objtop3
+            // 
+            this.objtop3.BackColor = System.Drawing.Color.Gold;
+            this.objtop3.Location = new System.Drawing.Point(201, 199);
+            this.objtop3.Name = "objtop3";
+            this.objtop3.Size = new System.Drawing.Size(50, 10);
+            this.objtop3.TabIndex = 21;
+            this.objtop3.TabStop = false;
+            // 
+            // objsid6
+            // 
+            this.objsid6.BackColor = System.Drawing.Color.Gold;
+            this.objsid6.Location = new System.Drawing.Point(250, 208);
+            this.objsid6.Name = "objsid6";
+            this.objsid6.Size = new System.Drawing.Size(10, 50);
+            this.objsid6.TabIndex = 20;
+            this.objsid6.TabStop = false;
+            // 
+            // objdwn3
+            // 
+            this.objdwn3.BackColor = System.Drawing.Color.Gold;
+            this.objdwn3.Location = new System.Drawing.Point(201, 256);
+            this.objdwn3.Name = "objdwn3";
+            this.objdwn3.Size = new System.Drawing.Size(50, 10);
+            this.objdwn3.TabIndex = 19;
+            this.objdwn3.TabStop = false;
+            // 
+            // objsid5
+            // 
+            this.objsid5.BackColor = System.Drawing.Color.Gold;
+            this.objsid5.Location = new System.Drawing.Point(192, 208);
+            this.objsid5.Name = "objsid5";
+            this.objsid5.Size = new System.Drawing.Size(10, 50);
+            this.objsid5.TabIndex = 18;
+            this.objsid5.TabStop = false;
+            // 
+            // limit
+            // 
+            this.limit.Location = new System.Drawing.Point(33, 432);
+            this.limit.Name = "limit";
+            this.limit.Size = new System.Drawing.Size(293, 26);
+            this.limit.TabIndex = 22;
+            this.limit.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(361, 450);
+            this.Controls.Add(this.limit);
+            this.Controls.Add(this.objtop3);
+            this.Controls.Add(this.objsid6);
+            this.Controls.Add(this.objdwn3);
+            this.Controls.Add(this.objsid5);
+            this.Controls.Add(this.objtop1);
+            this.Controls.Add(this.objsid2);
+            this.Controls.Add(this.objdwn1);
+            this.Controls.Add(this.objsid1);
+            this.Controls.Add(this.objtop2);
+            this.Controls.Add(this.objsid4);
+            this.Controls.Add(this.objdwn2);
+            this.Controls.Add(this.objsid3);
+            this.Controls.Add(this.barratop);
+            this.Controls.Add(this.barrader);
+            this.Controls.Add(this.barraizq);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.paletader);
+            this.Controls.Add(this.paletaizq);
+            this.Controls.Add(this.score);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Pinball";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdwn);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.paletaizq)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paletader)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barraizq)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barrader)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barratop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objsid3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objdwn2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objsid4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objtop2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objtop1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objsid2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objdwn1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objsid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objtop3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objsid6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objdwn3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objsid5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.limit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox paletaizq;
+        private System.Windows.Forms.PictureBox paletader;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Timer gametimer;
+        private System.Windows.Forms.PictureBox barraizq;
+        private System.Windows.Forms.PictureBox barrader;
+        private System.Windows.Forms.PictureBox barratop;
+        private System.Windows.Forms.PictureBox objsid3;
+        private System.Windows.Forms.PictureBox objdwn2;
+        private System.Windows.Forms.PictureBox objsid4;
+        private System.Windows.Forms.PictureBox objtop2;
+        private System.Windows.Forms.PictureBox objtop1;
+        private System.Windows.Forms.PictureBox objsid2;
+        private System.Windows.Forms.PictureBox objdwn1;
+        private System.Windows.Forms.PictureBox objsid1;
+        private System.Windows.Forms.PictureBox objtop3;
+        private System.Windows.Forms.PictureBox objsid6;
+        private System.Windows.Forms.PictureBox objdwn3;
+        private System.Windows.Forms.PictureBox objsid5;
+        private System.Windows.Forms.PictureBox limit;
+    }
+}
+
---
+++ Pinball proof of concept/Pinball proof of concept/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pinball_proof_of_concept
+{
+    public partial class Form1 : Form
+    {
+        int speed = 5; // integer called speed holding value of 5
+        int bally = 15; // horizontal X speed value for the ball object 
+        int ballx = 5; // vertical Y speed value for the ball object
+        int scores = 0; // score for the player
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gametimer_Tick(object sender, EventArgs e)
+        {
+
+            // if the ball hits the player or the CPU
+            if (ball.Bounds.IntersectsWith(paletader.Bounds) || ball.Bounds.IntersectsWith(paletaizq.Bounds))
+            {
+                // then bounce the ball in the other direction
+                ballx = -ballx;
+            }
+
+            score.Text = "" + scores; // show player score on label 1
+            
+            ball.Top -= bally; // assign the ball TOP to ball Y integer
+            ball.Left -= ballx; // assign the ball LEFT to ball X integer
+
+            if (ball.Bounds.IntersectsWith(barrader.Bounds)||
+                ball.Bounds.IntersectsWith(barraizq.Bounds)
+               )
+            {
+                ballx = -ballx;
+            }
+
+            if (ball.Bounds.IntersectsWith(barratop.Bounds)||
+                ball.Bounds.IntersectsWith(paletader.Bounds)||
+                ball.Bounds.IntersectsWith(paletaizq.Bounds)
+                )
+            {
+                bally = -bally;
+            }
+
+            if (ball.Bounds.IntersectsWith(limit.Bounds))
+            {
+                endGame();
+            }
+
+            if (ball.Bounds.IntersectsWith(objsid1.Bounds) ||
+                ball.Bounds.IntersectsWith(objsid2.Bounds) ||
+                ball.Bounds.IntersectsWith(objsid3.Bounds) ||
+                ball.Bounds.IntersectsWith(objsid4.Bounds) ||
+                ball.Bounds.IntersectsWith(objsid5.Bounds) ||
+                ball.Bounds.IntersectsWith(objsid6.Bounds))
+            {
+                ballx = -ballx;
+                scores++;
+            }
+
+            if(ball.Bounds.IntersectsWith(objtop1.Bounds) ||
+                ball.Bounds.IntersectsWith(objtop2.Bounds) ||
+                ball.Bounds.IntersectsWith(objtop3.Bounds) ||
+                ball.Bounds.IntersectsWith(objdwn1.Bounds) ||
+                ball.Bounds.IntersectsWith(objdwn2.Bounds) ||
+                ball.Bounds.IntersectsWith(objdwn3.Bounds))
+            {
+                bally = -bally;
+                scores++;
+            }
+        }
+        private void endGame()
+        {
+            gametimer.Stop();
+            score.Text += " Game Over";
+        }
+
+        private void keyisdwn(object sender, KeyEventArgs e)
+        {
+           if(e.KeyCode == Keys.Right)
+            {
+                paletader.Top = 374;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                paletaizq.Top = 374;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                paletader.Top = 407;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                paletaizq.Top = 407;
+            }
+        }
+    }
+}
